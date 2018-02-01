a4c81824e724687aa688a2f24d46a9da1ffd1903
mmatthew <takeknock@gmail.com>
2018-02-01 20:21:15 +0900

    add routing and db tables

--- myblogapp/posts/views.py
+++ myblogapp/posts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello World!")
-    return render(request, 'posts/index.html')
+    posts = Post.objects.order_by('-published')
+    return render(request, 'posts/index.html', {'posts' : posts})
